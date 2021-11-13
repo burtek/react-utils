13ef0545373e3fbefa70994022fbcb3e801612ca
Bartosz Dotryw <burtekdotryw@gmail.com>
2021-11-13 22:32:15 +0100

    more tests

--- src/hooks/useObject.test.ts
+++ src/hooks/useObject.test.ts
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { renderHook } from '@testing-library/react-hooks';
 import { useObject } from './useObject';
 
 describe('useObject', () => {
     const instance1 = { n: 1 };
     const instance2 = { n: 2 };
+
+    it.each([
+        123,
+        'string',
+        undefined
+    ])('should throw if input is %s', input => {
+        expect(() => {
+            const { result } = renderHook(() => useObject(input as any));
+            return result.current;
+        }).toThrowError(/^useObject requires an object/);
+    });
+    it('should throw if input is array', () => {
+        expect(() => {
+            const { result } = renderHook(() => useObject([]));
+            return result.current;
+        }).toThrowError(/^Use useArray for arrays/);
+    });
 
     it('should return same instance on first and next render', () => {
         const { rerender, result } = renderHook(() => useObject(instance1));
