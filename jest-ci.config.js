17408fa12203aa62f334b6dab19560e1d0d84987
Bartosz Dotryw <burtekdotryw@gmail.com>
2021-11-13 23:31:40 +0100

    try test results reporter

--- jest-ci.config.js
+++ jest-ci.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const config = {
-    ci: true
+    ci: true,
+    reporters: ["default", "jest-junit"]
 };
 
 module.exports = Object.assign({}, require('./jest-coverage.config.js'), config);
